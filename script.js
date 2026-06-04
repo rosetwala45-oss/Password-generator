9a6227c11069ce4862b5b944ce30686d29dacfc9
rosetwala45-oss <rosetwala45@gmail.com>
2026-06-04 16:57:19 +0200

    added page structure

--- script.js
+++ script.js
@@ -7,3 +7,12 @@
 // prettier-ignore
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let passwordEl1 = document.getElementById("password-1");
+let passwordEl2 = document.getElementById("password-2");
+
+function generate() {
+  let random = Math.floor(Math.random() * characters.length);
+  console.log(random);
+}
+generate();

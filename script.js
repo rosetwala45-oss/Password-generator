69c5cd6ee8f0863786e41fe8e8a74fa2ea60fb76
rosetwala45-oss <rosetwala45@gmail.com>
2026-06-05 22:49:03 +0200

    update function
    had to move array variables into functions to make them local to scope so that varable can reset eveytime the function finishes running.
    
    Local variables are not stored throughout. Adventageous to use in cases where variables are constantly being changed or need to be frequently updated.

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 // prettier-ignore
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let characterArray1 = [];
-let characterArray2 = [];
+
 let length = 15;
 
+let generateEl = document.getElementById("generate-btn");
 let passwordEl1 = document.getElementById("password-1");
 let passwordEl2 = document.getElementById("password-2");
 
@@ -19,28 +19,30 @@ function getRandomCharacter() {
   return characters[randomIndex];
 }
 
-function generatePasswords() {
-  passwordEl1.textContent = getPasswordString1();
-  passwordEl2.textContent = getPasswordString2();
+function updatePasswords() {
+  generatePasswords();
 }
 
-// ----------Password 1
-function getPasswordString1() {
+function generatePasswords() {
+  // ----------Password 1
+  let characterArray1 = [];
   let passwordString1 = "";
 
   for (let i = 0; i < length; i++) {
     characterArray1.push(getRandomCharacter());
     passwordString1 += characterArray1[i];
   }
-  return passwordString1;
-}
-// ----------Password 2
-function getPasswordString2() {
+  passwordEl1.textContent = passwordString1;
+
+  // ----------Password 2
+  let characterArray2 = [];
   let passwordString2 = "";
 
   for (let i = 0; i < length; i++) {
     characterArray2.push(getRandomCharacter());
     passwordString2 += characterArray2[i];
   }
-  return passwordString2;
+  passwordEl2.textContent = passwordString2;
 }
+
+generateEl.addEventListener("click", updatePasswords);

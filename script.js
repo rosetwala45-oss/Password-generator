f938063ac6cbf445a4e6a5a51389d211011435dd
rosetwala45-oss <rosetwala45@gmail.com>
2026-06-07 16:09:19 +0200

    Refactor password input layout and styles, update length handling

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // prettier-ignore
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let length = 20;
+let length = document.forms[0];
 let generateEl = document.getElementById("generate-btn");
 let passwordEl1 = document.getElementById("password-1");
 let passwordEl2 = document.getElementById("password-2");
@@ -24,7 +24,7 @@ function generatePasswords() {
   let characterArray1 = [];
   let passwordString1 = "";
 
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < length.value; i++) {
     characterArray1.push(getRandomCharacter());
     passwordString1 += characterArray1[i];
   }
